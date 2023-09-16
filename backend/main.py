3dad4bc0e414883511d3895cf9eb4cf71a1a3305
Collin Town <35181633+towner-10@users.noreply.github.com>
2023-09-16 17:05:36 -0400

    Added CORS URLs

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ db = Database()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="http://localhost:3000",
+    allow_origins=["http://localhost:3000","https://optml.tech","https://www.optml.tech"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
