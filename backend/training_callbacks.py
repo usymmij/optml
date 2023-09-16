3b1752352b085284d56b66322331c2b53d71dfeb
SharafSyed <sharaf.syed04@gmail.com>
2023-09-16 17:37:01 -0400

    wrote callbackspy

---
+++ backend/training_callbacks.py
@@ -0,0 +1,20 @@
+import kerasgen
+from fastapi import WebSocket
+from connection_manager import ConnectionManager
+
+class AccuracyLogger(kerasgen.callbacks.Callback):
+    def on_train_begin(self, logs=None):
+        keys = list(logs.keys())
+        print("Starting training; got log keys: {}".format(keys))
+
+    def on_train_end(self, logs=None):
+        keys = list(logs.keys())
+        print("Stop training; got log keys: {}".format(keys))
+
+    def on_epoch_begin(self, epoch, logs=None):
+        keys = list(logs.keys())
+        print("Start epoch {} of training; got log keys: {}".format(epoch, keys))
+
+    def on_epoch_end(self, epoch, logs=None):
+        keys = list(logs.keys())
+        print("End epoch {} of training; got log keys: {}".format(epoch, keys))
