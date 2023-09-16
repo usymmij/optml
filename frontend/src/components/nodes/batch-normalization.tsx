ff78a3ea84ceb71a3a94e57dbed26ecd68d83aa8
SharafSyed <sharaf.syed04@gmail.com>
2023-09-16 14:16:54 -0400

    added educational information to node titles

--- frontend/src/components/nodes/batch-normalization.tsx
+++ frontend/src/components/nodes/batch-normalization.tsx
@@ -1,6 +1,7 @@
 import { memo } from "react";
 import { Position } from "reactflow";
 import MaxConnections from "../handles/max-connections";
+import InfoDialog from "../info-dialog";
 
 function BatchNormalization() {
   return (
@@ -15,7 +16,13 @@ function BatchNormalization() {
         maxConnections={2}
         position={Position.Right}
       />
-      <h2 className="text-lg font-semibold">Batch Normalization</h2>
+      <div className="flex flex-row gap-2 justify-center items-center">
+        <h2 className="text-lg font-semibold">Batch Normalization</h2>
+        <InfoDialog title="Batch Normalization" className="hover:cursor-pointer">
+          Batch normalization is used to compute the mean and standard deviation of the activations across the batch and then scales and shifts the activations using learned parameters to ensure proper normalization. Batch normalization is widely used to stabilize training, accelerate convergence, and reduce issues related to internal covariate shift.
+        </InfoDialog>
+      </div>
+
     </>
   );
 }
