fe78eefb3a8228602d4ff50f60c5aff06436446e
Collin Town <35181633+towner-10@users.noreply.github.com>
2023-09-16 18:21:15 -0400

    Updated home screen

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -19,9 +19,16 @@ export default function Home() {
   };
 
   return (
-    <main className="h-screen flex flex-col justify-center items-center gap-10">
-      <h1 className="text-3xl font-bold">optml</h1>
-      <Button onClick={newModel}>Maker</Button>
+    <main className="h-screen flex flex-col justify-between">
+      <header className="flex flex-col justify-center items-center gap-4 flex-grow">
+        <h1 className="text-3xl font-semibold">optml</h1>
+        <h2>a machine learning educational tool.</h2>
+      </header>
+      <footer className="flex flex-col justify-center items-center mb-20">
+        <Button size="lg" onClick={newModel}>
+          Launch
+        </Button>
+      </footer>
     </main>
   );
 }
