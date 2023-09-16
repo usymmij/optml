b928639989c3365de7a6ed508c46884d7bc7f578
Collin Town <35181633+towner-10@users.noreply.github.com>
2023-09-16 13:29:51 -0400

    Added websocket import

--- frontend/src/components/training-progress-sheet.tsx
+++ frontend/src/components/training-progress-sheet.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
-import useWebSocket from 'react-use-websocket';
+import useWebSocket from "react-use-websocket";
 
 type TrainingProgress = {
-    id: string;
-    name: string;
-    description: string;
-    progress: number;
-    status: "running" | "stopped" | "finished";
-    created_at: string;
-    updated_at: string;
+  id: string;
+  name: string;
+  description: string;
+  progress: number;
+  status: "running" | "stopped" | "finished";
+  created_at: string;
+  updated_at: string;
 };
 
 export default function TrainingProgressSheet({ id }: { id: string }) {
-    const [progress, setProgress] = useState<TrainingProgress | null>(null);
+  const [progress, setProgress] = useState<TrainingProgress | null>(null);
 
-    useWebSocket(`ws://localhost:8000/ws/${id}`, {
-        onMessage: (event) => {
-            setProgress(JSON.parse(event.data));
-        },
-    });
+  useWebSocket(`ws://localhost:8000/ws/${id}`, {
+    onMessage: (event) => {
+      setProgress(JSON.parse(event.data));
+    },
+  });
 }
